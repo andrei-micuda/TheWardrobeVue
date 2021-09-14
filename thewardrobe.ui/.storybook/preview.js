0013beba752981a6389208f62ed3d269d1c4598f
Micu <andrei.micuda@gmail.com>
2021-09-14 23:48:49 +0300

    Initial Commit

---
+++ thewardrobe.ui/.storybook/preview.js
@@ -0,0 +1,11 @@
+import { app } from '@storybook/vue3'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
---
+++ thewardrobe.ui/src/components/FavoriteButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Icon
+      :icon="this.icon"
+      :width="size"
+      :color="this.color"
+      @click="toggleFavorite()"
+    />
+  </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+
+export default {
+  name: "FavoriteButton",
+  props: {
+    size: String,
+  },
+  data() {
+    return {
+      color: "gray",
+      icon: "ci:heart-outline",
+    };
+  },
+  methods: {
+    toggleFavorite() {
+      this.color = this.color === "red" ? "gray" : "red";
+      this.icon =
+        this.icon === "ci:heart-outline" ? "ci:heart-fill" : "ci:heart-outline";
+    },
+  },
+  components: { Icon },
+};
+</script>
+
+<style scoped>
+div {
+  cursor: pointer;
+  display: inline;
+}
+</style>
