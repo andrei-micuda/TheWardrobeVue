2ab54072f975865138e485186eea2b2a2b02c2d7
Micu <andrei.micuda@gmail.com>
2021-12-03 16:53:48 +0200

    Test

--- TheWardrobe.API/Startup.cs
+++ TheWardrobe.API/Startup.cs
@@ -71,7 +71,7 @@ namespace TheWardrobe.API
                               builder =>
                               {
                                 builder.WithOrigins("https://localhost:8080");
-                                builder.WithOrigins("http://172.17.0.5:3000");
+                                builder.WithOrigins("http://localhost:3000");
                               });
       });
 
