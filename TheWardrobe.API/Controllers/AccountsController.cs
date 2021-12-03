8ea9d17b174ec0f286c0b6f3e73bdd9cb8acaf69
Micu <andrei.micuda@gmail.com>
2021-12-03 19:52:02 +0200

    Adding serilog to api

--- TheWardrobe.API/Controllers/AccountsController.cs
+++ TheWardrobe.API/Controllers/AccountsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Serilog;
 using TheWardrobe.API.Entities;
 using TheWardrobe.API.Helpers;
 using TheWardrobe.API.Models.Accounts;
--- TheWardrobe.API/Program.cs
+++ TheWardrobe.API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using TheWardrobe.API.Helpers;
 using TheWardrobe.API.Migrations;
 
@@ -28,6 +29,12 @@ namespace TheWardrobe.API
       }
 
       SimpleCRUD.SetDialect(SimpleCRUD.Dialect.PostgreSQL);
+
+      Log.Logger = new LoggerConfiguration()
+                  .MinimumLevel.Verbose()
+                  .Enrich.FromLogContext()
+                  .WriteTo.Console()
+                 .CreateLogger();
 
       CreateHostBuilder(args).Build().Run();
     }
